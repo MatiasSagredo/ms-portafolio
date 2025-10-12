f65467d30d5a8980290554a254374b0f92c198a3
MatiasSagredo <ma.sagredom@duocuc.cl>
2025-10-12 13:00:40 -0300

    remover y editar cosas de proyecto y plantilla

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    cssMinify: 'lightningcss',
+    target: ['es2020']
+  },
+  css: {
+    transformer: 'lightningcss'
+  }
 })
